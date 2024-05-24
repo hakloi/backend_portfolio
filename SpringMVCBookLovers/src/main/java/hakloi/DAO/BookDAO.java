74637b8c97b06b00e0f6b80314775a36923938b3
Violetta <murasakitta@gmail.com>
2024-05-24 07:59:32 +0300

    update: postgresql

--- SpringMVCBookLovers/src/main/java/hakloi/DAO/BookDAO.java
+++ SpringMVCBookLovers/src/main/java/hakloi/DAO/BookDAO.java
@@ -3,41 +3,74 @@ package hakloi.DAO;
 import hakloi.model.Book;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class BookDAO {
     private static int id;
-    private List<Book> books;
 
-    {
-        books = new ArrayList<>();
-        books.add(new Book(++id, "Wuthering Heights", "Her only novel is the story of inseparable soulmates Heathcliff and Catherine Earnshaw and how betrayal, revenge and brutality led them down a dark path.", "Emily Brontë."));
-        books.add(new Book(++id, "Jane Eyre", "The novel focuses on the titular character as she comes of age, finds her way into the social class, and finds love and happiness with her brooding master.", "Charlotte Brontë"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "1608";
+
+    private static Connection connection1;
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Book> index(){
+        List<Book> books = new ArrayList<>();
+        try {
+            Statement statement = connection1.createStatement();
+            String SQL = "SELECT * FROM Book";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()){
+                Book book = new Book();
+                book.setId(resultSet.getInt("id"));
+                book.setName(resultSet.getString("name"));
+                book.setAuthor(resultSet.getString("author"));
+                book.setDescr(resultSet.getString("descr"));
+
+                books.add(book);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return books;
     }
 
     public Book show(int id){
-        return books.stream().filter(book -> book.getId() == id).findAny().orElse(null);
+//        return books.stream().filter(book -> book.getId() == id).findAny().orElse(null);
+        return null;
     }
 
     public void save(Book book){
-        book.setId(++id);
-        books.add(book);
+//        book.setId(++id);
+//        books.add(book);
     }
 
     public void update(int id, Book updatedBook){
-        Book bookToBeUpdated = show(id);
-
-        bookToBeUpdated.setName(updatedBook.getName());
-        bookToBeUpdated.setAuthor(updatedBook.getAuthor());
-        bookToBeUpdated.setDescr(updatedBook.getDescr());
+//        Book bookToBeUpdated = show(id);
+//
+//        bookToBeUpdated.setName(updatedBook.getName());
+//        bookToBeUpdated.setAuthor(updatedBook.getAuthor());
+//        bookToBeUpdated.setDescr(updatedBook.getDescr());
     }
 
     public void delete(int id){
-        books.removeIf(b -> b.getId() == id);
+//        books.removeIf(b -> b.getId() == id);
     }
 }
